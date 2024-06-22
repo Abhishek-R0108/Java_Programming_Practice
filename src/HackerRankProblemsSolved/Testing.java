5a99658bc964a3ab8faa77f636d0fc51228e56b5
Abhishek-R0108 <abhishekr0108@gmail.com>
2024-06-22 15:50:56 +0530

    failure fix

--- src/HackerRankProblemsSolved/Testing.java
+++ src/HackerRankProblemsSolved/Testing.java
@@ -7,7 +7,7 @@ public class Testing {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
-        if(number/2 == 0) {
+        if(number%2 == 0) {
             System.out.println("even");
         } else {
             System.out.println("odd");
