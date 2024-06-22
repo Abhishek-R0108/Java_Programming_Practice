d74e428f00e454305cc604b13a0b02f33b35e2c9
Abhishek-R0108 <abhishekr0108@gmail.com>
2024-06-22 07:59:40 +0530

    Day1.0

---
+++ src/Programs/DataTypeRelatedPgms.java
@@ -0,0 +1,41 @@
+//Diff btw Primitive and Object oriented(Non-Primitive) Datatype.
+package Programs;
+
+import java.util.Arrays;
+
+public class DataTypeRelatedPgms {
+    public static void main(String[] args) {
+        // Primitive Data types
+        /*When a primitive data type is stored, it is the stack that the values
+        will be assigned. When a variable is copied then another copy of the
+        variable is created and changes made to the copied variable will not reflect
+        changes in the original variable.*/
+
+        int a = 30;
+        int b = a; // value changed to 30
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+
+        // we will set the new value of b
+        b = 20;
+        System.out.println("The new value set to B should not affect the value A");
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+
+        //NON-PRIMITIVE DATA TYPE
+        /*In Object data type although two copies will be created they both will point
+        to the same variable in the heap, hence changes made to any variable will reflect
+        the change in both the variables.*/
+
+        int[] c = {10, 20, 30, 40};
+        int[] d = c;
+        System.out.println("The array values of c and d before:");
+        System.out.println("C = " + Arrays.toString(c));
+        System.out.println("D = " + Arrays.toString(d));
+
+        System.out.println("The array values of c and d after:");
+        d[2] = 9;
+        System.out.println("C = " + Arrays.toString(c));
+        System.out.println("D = " + Arrays.toString(d));
+    }
+}
---
+++ src/Programs/SumAndProduct.java
@@ -0,0 +1,23 @@
+//This program calculates sum and product of the number given by user.
+package Programs;
+
+import java.util.Scanner;
+
+public class SumAndProduct {
+    public static void main(String[] args) {
+        int a, b, sum, product;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        a = input.nextInt();
+        System.out.println("Enter the second number:");
+        b = input.nextInt();
+        sum = a + b;
+        product = a * b;
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+        System.out.println("The product of " + a + " and " + b + " is " + product);
+        //Average of the sum
+        float average = (float)sum/2;
+        System.out.println("The average of the sum " + sum + " is " + average);
+    }
+
+}
